b3c6f44769e0521c2e3a3e8b27b9a68fc9849d56
nxtanan <67568809+nxtanan@users.noreply.github.com>
2021-05-18 17:06:38 +0700

    Update SongComponent.js

--- components/SongComponent.js
+++ components/SongComponent.js
@@ -36,7 +36,7 @@ const SongComponent = ({route, navigation}) => {
       .finally(() => setLoading(false));
   }, [songID]);
 
-  const imageID = songID > 10 ? `7${songID}` : `70${songID}`;
+  const imageID = songID >= 10 ? `7${songID}` : `70${songID}`;
   const imageSource = {uri: `https://picsum.photos/${imageID}`};
   const renderLeftAvatar = () => (
     <Avatar.Image size={50} source={imageSource} />
